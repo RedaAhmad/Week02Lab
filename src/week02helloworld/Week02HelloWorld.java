54b48da78003a1a5cf25f70bc3b94490033eb37a
reda_ <reda_@DESKTOP-0K4CRAG>
2023-01-22 14:03:35 -0700

    New Project Creation Create a new Java Application for Week02HelloWorld

---
+++ src/week02helloworld/Week02HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02helloworld;
+
+/**
+ *
+ * @author reda_
+ */
+public class Week02HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hellow World!");
+        // TODO code application logic here
+    }
+    
+}
