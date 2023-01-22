2441a9df0d3e8f3960c72cab1acad38e62164504
reda_ <reda_@DESKTOP-0K4CRAG>
2023-01-22 14:05:53 -0700

    New Project Creation Create a new Java Application for Week02HelloWorld

--- src/week02helloworld/Week02HelloWorld.java
+++ src/week02helloworld/Week02HelloWorld.java
@@ -15,7 +15,7 @@ public class Week02HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hellow World!");
+        System.out.print("Hello World!");
         // TODO code application logic here
     }
     
